509827e3448c2b81e02ffae0a030d22f6f3fe8f5
Marshall Houston <marshall.houston@gmail.com>
2018-02-18 13:53:51 -0700

    Initial commit.

---
+++ spec/support/factories/factory_bot.rb
@@ -0,0 +1,8 @@
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
+# for each model, define a factory.
+# FactoryBot.define do
+#   <factories for each model go here>
+# end
