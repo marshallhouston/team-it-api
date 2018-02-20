b2e2ebcac885d5b6915b5d5216cbef1be7e44ad6
Marshall Houston <marshall.houston@gmail.com>
2018-02-19 20:11:55 -0700

    Add create method for teams. Phone numbers created here are a stand-in for the Twilio service that will be added

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -16,4 +16,26 @@ class Api::V1::TeamsController < ApplicationController
     end
   end
 
+  def create
+    team = Team.new(new_team_params)
+
+    if team.save
+      render json: TeamSerializer.new(team).serialized_json, status: :created
+    else
+      render json: team.errors, status: 400
+    end
+  end
+
+    private
+
+    def team_params
+      params.require(:team).permit(:name)
+    end
+
+    def new_team_params
+      # for proof of concept/proper rendering via client, I'm generating a random phone number.
+      # this is where the Twilio service will create a # for the team and then save it in the db.
+      phone_number = {phone: Faker::PhoneNumber.phone_number }
+      team_params.merge(phone_number)
+    end
 end
