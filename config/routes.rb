f0c8f71286203788c1d10de7ee8906364195d9e3
Marshall Houston <marshall.houston@gmail.com>
2018-02-19 20:12:27 -0700

    Change naming in routes for teams index, show, and create

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/teams', to: 'teams#index'
-      get '/teams/:id', to: 'teams#show'
+      resources :teams, only: [:index, :show, :create]
     end
   end
 end
