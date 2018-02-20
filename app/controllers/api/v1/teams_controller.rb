a1fba6c160e326ad61fc470da64283edc2556fc3
Marshall Houston <marshall.houston@gmail.com>
2018-02-19 17:16:01 -0700

    add route and controller action for showing a team

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -7,4 +7,13 @@ class Api::V1::TeamsController < ApplicationController
     render json: TeamSerializer.new(teams, options).serialized_json
   end
 
+  def show
+    team = Team.find_by(id: params[:id])
+    if team
+      render json: TeamSerializer.new(team).serialized_json
+    else
+      render json: {"Error": "Team not found"}, status: 404
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-
       get '/teams', to: 'teams#index'
-
+      get '/teams/:id', to: 'teams#show'
     end
   end
 end
