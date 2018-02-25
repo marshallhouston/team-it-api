8dfb140702617502bb8b22390651294ae8130377
Marshall Houston <marshall.houston@gmail.com>
2018-02-25 14:53:55 -0700

    Add tests for valid and invalid follower phone number.

--- spec/requests/api/v1/team_followers.rb
+++ spec/requests/api/v1/team_followers.rb
@@ -8,12 +8,28 @@ describe 'Team Followers API' do
       expect {
         post "/api/v1/teams/#{team.id}/followers", params: '{"follower": {"name": "Marshall Raymond", "phone": "555-555-5511", "email": "marshall@marshall.com"} }', headers: headers
       }.to change(team.users, :count).by(1)
+
+      parsed_info = JSON.parse(response.body)
+
       expect(response.status).to eq 201
+      expect(parsed_info['included'][0]['attributes']['name']).to eq 'Marshall Raymond'
+      expect(parsed_info['included'][0]['attributes']['phone']).to eq '+15555555511'
+      expect(parsed_info['included'][0]['attributes']['email']).to eq 'marshall@marshall.com'
     end
 
     context 'if a team does not exist' do
       it 'renders a 404 error' do
         post "/api/v1/teams/989/followers"
+        expect(response.status).to eq 404
+      end
+    end
+
+    context 'if a follower has an incorrectly formatted number' do
+      it 'returns a 404 error' do
+        team = create(:team)
+        headers = { "CONTENT_TYPE" => "application/json" }
+        post "/api/v1/teams/#{team.id}/followers", params: '{"follower": {"name": "Marshall Raymond", "phone": "555-555-551", "email": "marshall@marshall.com"} }', headers: headers
+
         expect(response.status).to eq 404
       end
     end
