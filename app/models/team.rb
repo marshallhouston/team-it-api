9a49beb3d2bcd874bc48f7cc40b914080c2a0bef
Marshall Houston <marshall.houston@gmail.com>
2018-02-25 11:52:43 -0700

    Update team model with team_code

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,11 @@
 class Team < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :team_code, presence: true, uniqueness: true
   has_many :team_followers
   has_many :users, through: :team_followers
+
+  def generate_team_code
+    self.team_code = Faker::Number.unique.number(3)
+  end
 
 end
