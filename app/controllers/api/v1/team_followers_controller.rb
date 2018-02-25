7129b6c369ed5a40a21b5ed8687fc5b54dffab28
Marshall Houston <marshall.houston@gmail.com>
2018-02-25 14:54:15 -0700

    Add error handling for invalid phone number with new follower

--- app/controllers/api/v1/team_followers_controller.rb
+++ app/controllers/api/v1/team_followers_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::TeamFollowersController < ApplicationController
 
   def create
-    if find_team
+    if find_team && find_follower
       @team.users << find_follower
-      render json: {"Message": "#{@team.name} added a new follower - #{@user.name}"}, status: :created
-    else
+      options = {}
+      options[:include] = [:users]
+      options[:meta] = {message: "#{@team.name} added a new follower - #{@user.name}"}
+      render json: TeamSerializer.new(@team, options).serialized_json, status: :created
+    elsif !find_team
       render json: {"Error": "Team not found"}, status: :not_found
+    else
+      render json: {"Error": "User info is incorrect. Check phone number for xxx-xxx-xxxx format."}, status: :not_found
     end
   end
 
@@ -16,9 +21,14 @@ class Api::V1::TeamFollowersController < ApplicationController
     end
 
     def find_follower
-      @user ||= User.find_or_create_by(email: follower_params[:email]) do |user|
-        user.name = follower_params[:name]
-        user.phone = follower_params[:phone]
+      @phone_number ||= PhoneFormatterService.format_number(follower_params[:phone])
+      if Phony.plausible?(@phone_number)
+        @user ||= User.find_or_create_by(email: follower_params[:email]) do |user|
+          user.name = follower_params[:name]
+          user.phone = @phone_number
+        end
+      else
+        false
       end
     end
 
